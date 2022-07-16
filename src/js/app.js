1f564bf667f1699c42107e6981793b3fa4eb5fa2
vincentb <v.bourdeau@bimedia.com>
2022-07-16 21:24:29 +0200

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-console.log('here in the app')
+const actionButton = document.querySelector('.advice-action')
+const adviceId = document.querySelector('.advice-id')
+const adviceSentence = document.querySelector('.advice-sentence')
+
+actionButton.addEventListener('click', () => getadvice())
+
+async function getadvice() {
+  const response = await fetch('https://api.adviceslip.com/advice')
+
+  if (!response.ok) {
+    adviceId.innerHTML = 'error'
+    adviceSentence.innerHTML = '--there was an error getting data, please try again--'
+    throw new Error(`An error has occured: ${response.status} => ${response.statusText}`)
+  }
+
+  const data = await response.json()
+
+  adviceId.innerHTML = data?.slip?.id ? data.slip.id : '0'
+  adviceSentence.innerHTML = data?.slip?.advice ? data.slip.advice : '--No advice for you today sweetheart--'
+}
+
